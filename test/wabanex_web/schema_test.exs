9962c809bea0ca4fc44b4b98bf0df158d12ad960
Isaac <isaac.kennedy@weclever.co>
2021-06-25 12:19:20 -0300

    Implement  User schema tests

---
+++ test/wabanex_web/schema_test.exs
@@ -0,0 +1,64 @@
+defmodule WabanexWeb.SchemaTest do
+  use WabanexWeb.ConnCase, async: true
+
+  alias Wabanex.User
+  alias Wabanex.Users.Create
+
+  describe "users queries" do
+    test "when a valid id is given, returns the user", %{conn: conn} do
+      params = %{email: "isaac@teste.com", name: "Isaac", password: "2345meia78"}
+
+      {:ok, %User{id: user_id}} = Create.call(params)
+
+      query = """
+        {
+          getUser(id: "#{user_id}") {
+            name
+            email
+          }
+        }
+      """
+
+      response =
+        conn
+        |> post("/api/graphql", %{query: query})
+        |> json_response(:ok)
+
+      expected_response = %{
+        "data" => %{
+          "getUser" => %{
+            "email" => "isaac@teste.com",
+            "name" => "Isaac"
+          }
+        }
+      }
+
+      assert response == expected_response
+    end
+  end
+
+  describe "users mutations" do
+    test "when all params are valid, creates the user", %{conn: conn} do
+      mutation = """
+        mutation {
+          createUser(input: {
+            name: "John Doe",
+            email: "john@doe.com",
+            password: "gojohnnygo"
+          }) {
+            name
+          }
+        }
+      """
+
+      response =
+        conn
+        |> post("/api/graphql", %{query: mutation})
+        |> json_response(:ok)
+
+      expected_response = %{"data" => %{"createUser" => %{"name" => "John Doe"}}}
+
+      assert response == expected_response
+    end
+  end
+end
